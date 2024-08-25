dbbc140bb97b145377d53c1618d86a2fcca20d37
Ravinder Kumar <hello.ravinderkumar@gmail.com>
2024-08-25 17:32:48 +0530

    project upload

---
+++ src/hooks/useAuth.js
@@ -0,0 +1,23 @@
+import { useSelector, useDispatch } from 'react-redux';
+
+import { setLogin, setLogout } from 'state/AppState';
+
+const useAuth = () => {
+
+    const auth = {
+        name: useSelector((state) => state.appData.name),
+      
+        email: useSelector((state) => state.appData.email),
+        token: useSelector((state) => state.appData.token),
+        role: useSelector((state) => state.appData.role),
+       
+        userData: useSelector((state) => state.appData.userData),
+      
+       
+    }
+
+    return { setLogin, setLogout,auth}
+}
+
+export default useAuth;
+
